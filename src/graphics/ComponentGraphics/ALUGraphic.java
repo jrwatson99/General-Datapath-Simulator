e3cc7a77ebdd664ca06a969b36055e17d8d5a522
matt02j <matt02j@email.arizona.edu>
2018-04-09 10:22:56 -0700

    added example mouse handler in alu

--- src/graphics/ComponentGraphics/ALUGraphic.java
+++ src/graphics/ComponentGraphics/ALUGraphic.java
@@ -97,13 +97,13 @@ public class ALUGraphic extends ComponentGraphic {
 
 	@Override
 	public void addMouseHandler() {shape.setOnMouseClicked(e-> { 
-		if(e.getButton().compareTo(MouseButton.SECONDARY)==0) {
-			this.config();
-		}
-		else if(e.getButton().compareTo(MouseButton.PRIMARY)==0) {
-			//TODO add click and drag;
-		}
-	} );
+			if(e.getButton().compareTo(MouseButton.SECONDARY)==0) {
+				this.config();
+			}
+			else if(e.getButton().compareTo(MouseButton.PRIMARY)==0) {
+				//TODO add click and drag;
+			}
+		} );
 		
 	}
 }
